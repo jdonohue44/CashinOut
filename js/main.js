233c7c8676a059b520e80969ced49c2b135b5e07
jdonohue44 <JaredDonohue@Jareds-MacBook-Pro.local>
2015-03-10 11:25:45 -0400

    jumpng
    jumping

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ window.onload = function() {
         //game.add.audio('theme').play();
         guy.animations.add('walk', [0,1,2,3,4,5,6,7,8,9,10,11,12], 13, true);
         guy.animations.add('run', [13,14,15,16,17,18,19,20,21,22], 13, true);
-        guy.animations.add('jump', [26,27,28,29,30,31,32,33,34,35,36,26],13,false);
+        guy.animations.add('jump', [27,28,29,30,31,32,33,34,35,36,26],13,false);
         guy.animations.add('stand', [26],13,false);
         cursors = game.input.keyboard.createCursorKeys();
         aButton = game.input.keyboard.addKey(Phaser.Keyboard.DOWN); // 90
