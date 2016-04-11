f676ba7a7a7f310287d2843973b6b61acb038324
jdonohue44 <jared.donohue@gmail.com>
2016-04-10 21:08:20 -0400

    Update Boot.js

--- Boot.js
+++ Boot.js
@@ -8,15 +8,12 @@ BasicGame.Boot.prototype = {
 
     init: function () {
     
-        //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
         this.input.maxPointers = 1;
-
-        //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
         this.stage.disableVisibilityChange = true;
 
         if (this.game.device.desktop)
         {
-            //  If you have any desktop specific settings, they can go in here
+            // any desktop specific settings can go here
             this.scale.pageAlignHorizontally = true;
         }
         else
